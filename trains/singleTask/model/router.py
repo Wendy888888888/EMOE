fdcd147c8e128cef38457e8d5e5ea752a42ece18
Wendy888888888 <142529912+Wendy888888888@users.noreply.github.com>
2025-10-27 20:00:07 +0800

    Update router.py

--- trains/singleTask/model/router.py
+++ trains/singleTask/model/router.py
@@ -1,3 +1,4 @@
+#Router:根据输入得到重要性权重：输入（2 张图片的特征：(2,64,8,8)）→ 展平成向量（(2,4096)）→ 降维（(2,512)）→ 归一化 + ReLU → 映射到 4 个专家（(2,4)）→ 温度调节 → 输出 4 个专家的权重概率（(2,4)）。
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
